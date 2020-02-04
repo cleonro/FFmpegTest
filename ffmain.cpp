fc6ac2e6c3e5b3b304819ab77ddc1604b5683015
catalin.ciobirca <catalin.ciobirca@adswizz.com>
2020-02-04 19:47:19 +0200

    added tutorials links.

--- ffmain.cpp
+++ ffmain.cpp
@@ -1,5 +1,16 @@
 #include "ffmainwindow.h"
 
+///TUTORIALS links
+/// 1.
+/// https://github.com/leandromoreira/ffmpeg-libav-tutorial
+///
+/// 2. (older)
+/// http://dranger.com/ffmpeg/
+///
+/// 3. (not investigated - yet)
+/// https://www.whoishostingthis.com/compare/ffmpeg/resources/
+
+
 #include <QApplication>
 
 int main(int argv, char *argc[])
